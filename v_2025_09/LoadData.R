8f90c9978e6a0fb9373249e824fbcc51021e5582
Shultz <Dana.Shultz@Waterboards.ca.gov>
2025-09-17 20:56:56 -0700

    Initial commit

---
+++ v_2025_09/LoadData.R
@@ -0,0 +1,76 @@
+# Load Data script for app_v2.R
+
+# Load libraries
+library(shiny)
+library(bslib) #add cards to format
+library(tidyverse)
+library(readxl) #read in excel files
+library(plotly) #interative plots
+library(leaflet) #map
+library(DT) #data table formatting
+
+# Import data for shiny
+df_st <- read_excel("inputs/rtd_sites.xlsx")
+df <- read.csv("inputs/rtd_lab_results.csv")
+
+# Import data if running driectly from this file
+# df_st <- read_excel("v_2025_09/inputs/rtd_sites.xlsx")
+# df <- read.csv("v_2025_09/inputs/rtd_lab_results.csv")
+
+# Sidebar blurbs
+sp1 <-
+  p(
+    "The Water Boards and Restore the Delta began a collaborative partnership
+    in 2022 to monitor HABs in the Delta near Stockton."
+    )
+
+sp2 <-
+  p(
+    "This dashboard was created by the Central Valley Water Board to
+    share laboratory data from HAB monitoring of the Delta near Stockton with 
+    the public and other agency staff."
+  )
+
+sp3 <- 
+  p(
+    #style = "text-align: center;",
+    "For questions, contact",
+    HTML(
+      "<a href='mailto:dana.shultz@waterboards.ca.gov' > dana.shultz@\nwaterboards.ca.gov</a>"
+    )
+  )
+
+# Set popup tooltip text for Stations map
+# set popup text
+tooltip_text <- paste0(
+  "<strong>","Site: ","</strong>", df_st$StationName, " (", df_st$StationID, ")" %>%
+  lapply(htmltools::HTML)
+)
+
+# Arrange data table to have most recent data at the top
+df_table <- df %>%
+  mutate(CollectionDate = as.Date(CollectionDate, format = "%m/%d/%Y")) %>% 
+  arrange(desc(CollectionDate))
+
+# Format data for plotting
+df_plot <- df %>% 
+  mutate(Year = factor(Year)) %>% 
+  #Set ND results to 0.01 for plotting 
+  mutate(`Total Microcystins` = case_when(`TotalMicrocystins` == "ND" ~ "0.07",
+                                          TRUE ~ `TotalMicrocystins`)) %>% 
+  mutate(`Total Microcystins` = as.numeric(`Total Microcystins`)) %>% 
+  # rename col with "_nd" for clarity, will use for plotly tooltip
+  mutate(total_microcystins_nd = TotalMicrocystins) %>%
+  select(-TotalMicrocystins) %>% 
+  mutate(CollectionDate = as.Date(CollectionDate, format = "%m/%d/%Y"))
+
+# Station names for checkbox input
+s <- unique(df_plot$Station)
+y <- unique(df_plot$Year)
+
+# Set some parameters for plotly scatter
+#Define shapes to use for Station icons
+my8shapes <- c(15,16,17,18,15,16,17,18)
+
+ymin = min(df_plot$`Total Microcystins` )
+ymax = max(df_plot$`Total Microcystins`)
---
+++ v_2025_09/app.R
@@ -0,0 +1,254 @@
+# Draft shiny app
+# 2025-09-08
+
+# load script that calls libraries and imports data and formats
+source("LoadData.R")
+
+# Define UI for application that draws a histogram
+ui <- page_navbar(
+
+    ### Application title
+    title = "Delta Community Data",
+    theme = bs_theme(
+      #hex codes from existing water board webpages
+      primary = "#046B99",
+      secondary = "#9FC7DA", 
+    ),
+
+    ### Sidebar with context and contact information
+    ### Text defined in LoadData.R
+    sidebar = sidebar(
+      sp1,
+      icon("flask", "fa-2x", align = "center", style = "color: #707070"),
+      sp2,
+      icon("chart-simple", "fa-2x", align = "center", style = "color: #707070"),
+      sp3,
+      includeHTML("html/sidebar_report_a_bloom_button.html")
+    ),
+    
+    ### Data tab
+    nav_panel(
+      
+      # Popup window when page is opened
+      modalDialog(
+        (
+          navset_tab(
+            nav_panel("Welcome",
+                      includeHTML("html/modal_dialog_welcome.html")),
+            nav_panel("Application",
+                      includeHTML("html/modal_dialog_application.html"))
+          )
+        ),
+        size = "l",
+        easyClose = TRUE
+      ), #end modal dialog
+      
+      class = "bslib-page-dashboard",
+      title = "Data",
+      
+      # Add text above graph/table/stations tabs
+      p("Laboratory results for total microcystin concentrations. Visit the Water Board ",
+        tags$a(href = "https://mywaterquality.ca.gov/habs/resources/reports-map/", 
+               "HAB Reports Map", target = "_blank"), " to view 
+                                the latest recreatonal HAB advisory levels."),
+      
+      # Create a card for graph/table/station tabs
+      navset_card_tab(
+        full_screen = TRUE,
+        
+        ### Graph tab (reactive)
+        nav_panel(
+          "Graph",
+          
+          layout_sidebar(
+            # sidebar with checbox inputs for stations and years
+            sidebar = sidebar(
+              checkboxGroupInput(
+                "stations_selected", "Select station(s):",
+                c(s),
+                selected = s
+              ),
+              checkboxGroupInput(
+                "years_selected", "Select year(s):",
+                c(y),
+                selected = y
+              )
+            ),
+            
+            # main panel with reactive plotly graph
+            plotlyOutput("xy_plot"),
+            "The ELISA reporting limit (dotted dark grey line) is 0.15 ug/L and non-detect (ND) samples
+            are plotted at 0.08 ug/L, half the reporting limit. HAB recreational advisory levels of 
+            Caution (Tier 1), Warning (Tier 2), and Danger (Tier 3) are plotted at 0.8, 6.0 and 20 ug/L 
+            as dashed grey lines."
+            
+          ) # end layout sidebar
+        ), # end Graph nav_panel tab
+        
+        ### Table tab (not reactive)
+        nav_panel(
+          "Table",
+          card_body(
+            DT::dataTableOutput("DataTable")
+          )
+        ),
+        
+        ### Map tab (not reactive)
+        nav_panel(
+          "Stations",
+          card_body(
+            leafletOutput(
+              "StationMap", height = 400
+              ),
+          )
+        )
+      )
+    ),
+    
+    ### About tab
+    nav_panel(
+      class = "bslib-page-dashboard",
+      title = "About",
+      img(src = "About_header.png", style = "right: 50%"),
+      card(
+        navlistPanel(
+          id = "inTabset",
+          fluid = TRUE,
+          widths = c(3,9),
+        tabPanel(
+          "Partners and Project Background",
+          includeHTML("html/about_partners.html")
+        ),
+        tabPanel(
+          "Methods",
+          includeHTML("html/about_methods.html")
+          ),
+        tabPanel(
+          "HABs in the Delta",
+          includeHTML("html/about_delta_habs.html")
+        )
+        )
+      )
+    ),
+    
+    # Related links in upper right of navigation menu
+    nav_spacer(),
+    nav_menu(
+      title = "Related Links",
+      align = "right",
+      nav_item(
+        tags$a("CV Water Board HAB webpage",
+               href = "https://www.waterboards.ca.gov/centralvalley/water_issues/harmful_algal_blooms/",
+               target = "blank")
+      ),
+      nav_item(
+        tags$a("Restore the Delta webpage",
+               href = "https://restorethedelta.org/",
+               target = "blank")
+      ),
+      nav_item(
+        tags$a("Water Board HAB Portal",
+               href = "https://mywaterquality.ca.gov/habs/",
+               target = "blank")
+      )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  ### reactive data frame based on station_selelction
+  df_sel <- reactive({
+
+    validate(
+      need(input$stations_selected != "", 'Please choose at least one station.')
+    )
+
+     validate(
+       need(input$years_selected != "", 'Please choose at least one year.')
+     )
+
+    file <- df_plot %>%
+      filter(Station %in% input$stations_selected) %>%
+      filter(Year %in% input$years_selected)
+    return(file)
+
+  })
+
+  ### reactive scatter plot of MC lab data based on station and year selection
+  output$xy_plot <- renderPlotly({
+    
+    p <- df_sel() %>%
+      ggplot(aes(x = CollectionDate, y = `Total Microcystins`, color = Station, shape = Station,
+                 text = paste("Collection Date: ", CollectionDate, 
+                              "<br>Total microcystins: ", total_microcystins_nd,
+                              "<br>Analyzed by: ", Method)))+
+      # grey lines for context at CCHAB trigger thresholds
+      geom_hline(yintercept = 20, linetype = "dashed", color = "grey", alpha = 0.5)+
+      geom_hline(yintercept = 6, linetype = "dashed", color = "grey", alpha = 0.5)+
+      geom_hline(yintercept = 0.8, linetype = "dashed", color = "grey", alpha = 0.5)+
+      geom_hline(yintercept = 0.15, linetype = "dotted", color = "#676767", alpha = 0.5)+
+      geom_point(size = 3, alpha = 0.5)+
+      scale_shape_manual(values = my8shapes)+
+      facet_wrap(vars(Year), nrow = 1, scales = "free_x")+
+      scale_y_log10(labels = scales::label_comma())+
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))+
+      labs(x = "Sample Date",
+           y = "Total Microcystins (ug/L)")
+    
+    #ggplotly(p, tooltip = c("CollectionDate", "total_microcystins_nd"))
+    ggplotly(p, tooltip = "text")
+    
+  })
+  
+  
+  
+  ### Data Table
+  output$DataTable <- DT::renderDataTable({
+    datatable(
+      data = df_table,
+      extensions = 'Buttons',
+      rownames = F,
+      options = list(
+        dom = "fBti",
+        pageLength = 5000,
+        buttons =
+          list(
+            list(
+              extend = "collection",
+              buttons = c("csv", "excel"),
+              text = "Download"
+            )
+          ) # end of buttons customization
+      ) # end of options
+    )
+  })
+  
+  
+  ### Station Map
+  output$StationMap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Esri.WorldImagery) %>% 
+      setView(lng = -121.356291, lat = 37.967723, zoom = 13) %>%
+      addCircleMarkers(data = df_st,
+                       radius = 12,
+                       color = "yellow",
+                       fillOpacity = 0.6,
+                       label = ~StationID,
+                       layerId = ~StationID,
+                       labelOptions = labelOptions(
+                         noHide = TRUE,# Keep labels always visible
+                         direction = "center",# Position label intelligently
+                         textOnly = TRUE,# Don't show speech bubble around text
+                         style = list("font-size" = "10px", "font-weight" = "bold","color"="black")
+                       ),
+                       popup = tooltip_text
+                       )
+  })
+  
+
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
